a379d19ee706f946177a9edff0ca1dc441c0e8c3
Dmitriy_Martys <12masek34@mail.ru>
2023-08-19 18:50:07 +0300

    include models in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,36 @@
-from django.contrib import admin
+from django.contrib.admin import (
+    ModelAdmin,
+    display,
+    register,
+    site,
+)
 
-# Register your models here.
+from app.models import (
+    Category,
+    District,
+    Enterprise,
+    NetworkEnterprise,
+    Product,
+    ProductPrice,
+)
+
+site.register(District)
+site.register(Category)
+site.register(NetworkEnterprise)
+site.register(Product)
+
+
+@register(Enterprise)
+class EnterpriseAdmin(ModelAdmin):
+    list_display = ("name", "network")
+    list_filter = list_display + ("product",)
+
+
+@register(ProductPrice)
+class ProductPriceAdmin(ModelAdmin):
+    list_display = ("product", "category", "price",)
+    search_fields = ("get_product",)
+
+    @display(description="Категория")
+    def category(self, obj):
+        return obj.product.category.name
