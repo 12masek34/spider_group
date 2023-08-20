2eda945a53b63dd0454c6619c0bae4291a219b71
Dmitriy_Martys <12masek34@mail.ru>
2023-08-20 12:06:51 +0300

    add token

--- app/views.py
+++ app/views.py
@@ -17,6 +17,9 @@ from rest_framework.mixins import (
     ListModelMixin,
     RetrieveModelMixin,
 )
+from rest_framework.permissions import (
+    IsAuthenticated,
+)
 from rest_framework.viewsets import (
     GenericViewSet,
     ModelViewSet,
@@ -40,15 +43,19 @@ class OrganizationByDistrictViewSet(ListModelMixin, RetrieveModelMixin, GenericV
     Возможно выполнить поиск по имени товара.
 
     Пример запроса -
-        curl --location 'http://127.0.0.1:8000/api/organizations/1/'
+        curl --location 'http://127.0.0.1:8000/api/organizations/1/' \
+        --header 'Authorization: Token 990d9608a0bd71c2d099ba121688a6194b4046a1'
 
     Пример запроса с фильтром по категории -
-        curl --location 'http://127.0.0.1:8000/api/organizations/3/?product__category__name=category_name'
+        curl --location 'http://127.0.0.1:8000/api/organizations/3/?product__category__name=category_name' \
+        --header 'Authorization: Token 990d9608a0bd71c2d099ba121688a6194b4046a1'
 
     Пример запроса с фильтром для поиска по имени продукта -
-        curl --location 'http://127.0.0.1:8000/api/organizations/3/?search=product_name'
+        curl --location 'http://127.0.0.1:8000/api/organizations/3/?search=product_name' \
+        --header 'Authorization: Token 990d9608a0bd71c2d099ba121688a6194b4046a1'
     """
     serializer_class = OrganizationSerializer
+    permission_classes = (IsAuthenticated,)
     http_method_names = ("get",)
     filter_backends = [SearchFilter, DjangoFilterBackend]
     filterset_fields = ("product__category__name",)
@@ -64,9 +71,11 @@ class OrganizationDetailAPIView(RetrieveAPIView):
     """Детальная информация по заведению.
 
     Пример запроса -
-        curl --location 'http://127.0.0.1:8000/api/organization/1'
+        curl --location 'http://127.0.0.1:8000/api/organization/1' \
+        --header 'Authorization: Token 990d9608a0bd71c2d099ba121688a6194b4046a1'
     """
     serializer_class = OrganizationSerializer
+    permission_classes = (IsAuthenticated,)
     queryset = Enterprise.objects.all()
     lookup_url_kwarg = "organization_id"
 
@@ -76,6 +85,7 @@ class ProductViewSet(ModelViewSet):
 
     Пример запроса добавления -
         curl --location 'http://127.0.0.1:8000/api/products/' \
+        --header 'Authorization: Token 990d9608a0bd71c2d099ba121688a6194b4046a1' \
         --header 'Content-Type: application/json' \
         --data '{
             "category": {
@@ -85,8 +95,10 @@ class ProductViewSet(ModelViewSet):
         }
         '
     Пример запроса детальной информации -
-        curl --location 'http://127.0.0.1:8000/api/products/1'
+        curl --location 'http://127.0.0.1:8000/api/products/1' \
+        --header 'Authorization: Token 990d9608a0bd71c2d099ba121688a6194b4046a1'
     """
     serializer_class = ProductCreateSerializer
+    permission_classes = (IsAuthenticated,)
     queryset = Product.objects.all()
     http_method_names = ("post", "get")
